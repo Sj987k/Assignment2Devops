0b3dae33fa5240c57b31ee7bf063ec9c0d639e13
c0856724 <c0856724@mylambton.ca>
2023-07-14 18:27:01 -0400

    update

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bootstrap_demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.BMI_791 = 0;
+            return View();
+        }
+
+        public IActionResult Index(BMI.Models.BMImodel_791 model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.BMI_791 = model.CalculateBMI_791();
+            }
+            else
+            {
+                ViewBag.BMI_791 = 0;
+            }
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/JashanBMI.cs
@@ -0,0 +1,20 @@
+﻿namespace BMI.Models
+{
+    public class BMImodel_791
+    {
+
+        public decimal? weight_791 { get; set; }
+
+        public decimal? height_791 { get; set; }
+
+        public decimal? CalculateBMI_791()
+        {
+            decimal? height = 0;
+            height = height_791 * height_791;
+            decimal? tempbmi = 0;
+            tempbmi = weight_791 / height;
+            decimal? BMI_791 = tempbmi * 703;
+            return BMI_791;
+        }
+    }
+}
